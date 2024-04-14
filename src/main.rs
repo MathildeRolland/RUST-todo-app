0844b5d7ca00750970e3ad619a2fb1111fd7b301
Mathilde Rolland <rolland.mmathilde@gmail.com>
2024-04-14 12:52:47 +0200

    task: handle todo completion

--- src/main.rs
+++ src/main.rs
@@ -28,32 +28,61 @@ fn view_todos(todos: &[Todo]) {
     }
 } 
 
+fn get_selected_todo(todos: &mut Vec<Todo>, chosen_todo_index: u32) -> Option<&mut Todo> {
+    let selected_todo = todos.into_iter().find(|todo| {
+        todo.id == chosen_todo_index
+    });
+    selected_todo
+}
+
+fn handle_todo_choice(todos: &mut Vec<Todo>, chosen_todo_index: u32) {
+    match chosen_todo_index {
+        1 => {
+            let selected_todo = get_selected_todo(todos, chosen_todo_index);
+            if selected_todo.is_some() {
+                todos[chosen_todo_index as usize - 1].completed = true;
+                println!("{:?}", todos);
+            }
+        },
+        2 => {},
+        3 => {},
+        _ => println!("Cette todo n'existe pas")
+     }
+    
+
+}
+
 fn main() {
     let mut todos = init_todos();
 
-    println!("Que souhaitez-vous faire?");
-    println!("1. Voir la liste des todos");
-    println!("2. Ajouter un todo");
+    loop {
 
-    let mut user_input = String::new();
-    io::stdin().read_line(&mut user_input).expect("Failed to read line");
-
-    let user_input_as_number = user_input.trim().parse::<u32>().expect("Invalid input");
-
-    match user_input_as_number {
-        1 => {
-            view_todos(todos.as_mut_slice());
-            let mut chosen_todo_index = String::new();
-            io::stdin().read_line(&mut chosen_todo_index).expect("Failed to read line");
-            let _chosen_todo_index_as_string = chosen_todo_index.trim().parse::<u32>().expect("Invalid input");
-        },
-        2 => {
-            println!("Veuillez renseigner le titre de votre nouvelle todo:");
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).expect("Failed to read line");
-            add_todo(&mut todos, input);
-        },
-        _ => {}
+        println!("Que souhaitez-vous faire?");
+        println!("1. Voir la liste des todos");
+        println!("2. Ajouter un todo");
+    
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).expect("Failed to read line");
+    
+        let user_input_as_number = user_input.trim().parse::<u32>().expect("Invalid input");
+    
+        match user_input_as_number {
+            1 => {
+                view_todos(todos.as_mut_slice());
+                let mut chosen_todo_index = String::new();
+                io::stdin().read_line(&mut chosen_todo_index).expect("Failed to read line");
+                let chosen_todo_index_as_number = chosen_todo_index.trim().parse::<u32>().expect("Invalid input");
+                handle_todo_choice(&mut todos, chosen_todo_index_as_number);
+            },
+            2 => {
+                println!("Veuillez renseigner le titre de votre nouvelle todo:");
+                let mut input = String::new();
+                io::stdin().read_line(&mut input).expect("Failed to read line");
+                add_todo(&mut todos, input);
+            },
+            _ => {}
+        }
     }
+
 
 }
