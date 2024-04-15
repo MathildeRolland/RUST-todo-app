19dd285729f7a917db11042a426b1d79baeea1bc
Mathilde Rolland <rolland.mmathilde@gmail.com>
2024-04-15 21:10:02 +0200

    refactor: Move the addTodo and viewTodos logic in a struct called TodosController. Instanciate this struct and call its methods instead of previous functions

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,6 @@
 pub mod todos;
-use todos::Todo;
+use todos::todos::{Todo, TodosController};
 use std::io;
-
-fn init_todos() -> Vec<Todo> { 
-    let mut todos: Vec<Todo> = Vec::new();
-
-    let todo1 = Todo {id: 1, label: String::from("Faire 100 pompes"), completed: false};
-    let todo2 = Todo {id: 2, label: String::from("Lire un livre"), completed: false};
-    let todo3 = Todo {id: 3, label: String::from("Postuler à 2 entreprises"), completed: false};
-
-    todos.push(todo1);
-    todos.push(todo2);
-    todos.push(todo3);
-
-    todos
-}
-
-fn add_todo(todos: &mut Vec<Todo>, input: String) {
-    let new_todo = Todo {id: 4, label: input, completed: false};
-    todos.push(new_todo);
-}
-
-fn view_todos(todos: &[Todo]) {
-    let todos_iter = todos.into_iter();
-    for todo in todos_iter {
-        println!("{}. [{}] {}", todo.id, if todo.completed {"x"} else {""}, todo.label);
-    }
-} 
 
 fn get_selected_todo(todos: &mut Vec<Todo>, chosen_todo_index: u32) -> Option<&mut Todo> {
     let selected_todo = todos.into_iter().find(|todo| {
@@ -72,7 +46,8 @@ fn handle_todo_choice(todos: &mut Vec<Todo>, chosen_todo_index: u32) {
 }
 
 fn main() {
-    let mut todos = init_todos();
+    let todos_controller = TodosController::new();
+    let mut todos = todos_controller.todos ;
 
     loop {
 
@@ -88,7 +63,7 @@ fn main() {
         match user_input_as_number {
             1 => {
                 loop {
-                    view_todos(todos.as_mut_slice());
+                    TodosController::view_todos(todos.as_mut_slice());
                     let mut chosen_todo_index = String::new();
                     io::stdin().read_line(&mut chosen_todo_index).expect("Failed to read line");
                     let chosen_todo_index_as_number = chosen_todo_index.trim().parse::<u32>().expect("Invalid input");
@@ -99,7 +74,7 @@ fn main() {
                 println!("Veuillez renseigner le titre de votre nouvelle todo:");
                 let mut input = String::new();
                 io::stdin().read_line(&mut input).expect("Failed to read line");
-                add_todo(&mut todos, input);
+                TodosController::add_todo(&mut todos, input);
             },
             _ => {}
         }
--- src/todos.rs
+++ src/todos.rs
@@ -1,9 +1,48 @@
 
-#[derive(Debug)]
-pub struct Todo {
-    pub id: u32,
-    pub label: String,
-    pub completed: bool
+pub mod todos {
+    #[derive(Debug)]
+    pub struct Todo {
+        pub id: u32,
+        pub label: String,
+        pub completed: bool
+    }
+
+    pub struct TodosController {
+        pub todos: Vec<Todo>
+    }
+
+    impl TodosController {
+        pub fn new() -> Self {
+            let mut todos: Vec<Todo> = Vec::new();
+            
+            let todo1 = Todo {id: 1, label: String::from("Faire 100 pompes"), completed: false};
+            let todo2 = Todo {id: 2, label: String::from("Lire un livre"), completed: false};
+            let todo3 = Todo {id: 3, label: String::from("Postuler à 2 entreprises"), completed: false};
+            
+            
+            todos.push(todo1);
+            todos.push(todo2);
+            todos.push(todo3);
+            
+            
+            Self {
+                todos,
+            }
+        }
+   
+        pub fn add_todo(todos: &mut Vec<Todo>, input: String) {
+            let new_todo = Todo {id: 4, label: input, completed: false};
+            todos.push(new_todo);
+        }
+
+        pub fn view_todos(todos: &[Todo]) {
+            let todos_iter = todos.into_iter();
+            for todo in todos_iter {
+                println!("{}. [{}] {}", todo.id, if todo.completed {"x"} else {""}, todo.label);
+            }
+        } 
+    }
+
 }
 
 
